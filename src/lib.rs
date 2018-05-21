8bda396ac9b83a9f9d6258bba5c11a634ac50160
andrewayoub <andrew.a.mikhaeel@gmail.com>
2018-05-21 10:55:10 +0200

    fix non value property issue

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ pub mod dmidecoder {
     fn clean_str(line :&str) -> String {
         let mut i = 0;
         for c in line.chars() {
-            if c.is_whitespace() || c == ':' {
+            if c.is_whitespace() {
                 i = i + 1;
             } else {
                 return line[i..].to_string();
@@ -129,13 +129,11 @@ pub mod dmidecoder {
                     if !current_property.is_empty() && !current_property_name.is_empty() {
                         current_section.properties.insert(current_property_name, current_property);
                         current_property = Property::new();
-                        current_property_name = String::new();
                     }
-                    let colon_index = line.find(':').unwrap_or(line.len());
-                    if colon_index != line.len()
-                    {
-                        current_property_name = clean_str(&line[..colon_index]);
-                        current_property.value = clean_str(&line[colon_index..]);
+                    let kvdata : Vec<&str> = line.split(':').collect();
+                    current_property_name = clean_str(kvdata[0]);
+                    if kvdata.len() > 1 {
+                        current_property.value = clean_str(kvdata[1]);
                     }
                 },
                 State::List => {
@@ -152,57 +150,5 @@ pub mod dmidecoder {
         return sections;
     }
 }
-#[cfg(test)]
-mod tests {
-    use dmidecoder::parse;
-    #[test]
-    fn it_works() {
-    let sample = r#"
-Getting SMBIOS data from sysfs.
-SMBIOS 2.7 present.
 
-Handle 0x0000, DMI type 0, 24 bytes
-BIOS Information
-	Vendor: LENOVO
-	Version: 9BCN26WW
-	Release Date: 07/31/2014
-	Address: 0xE0000
-	Runtime Size: 128 kB
-	ROM Size: 4096 kB
-	Characteristics:
-		PCI is supported
-		BIOS is upgradeable
-		BIOS shadowing is allowed
-		Boot from CD is supported
-		Selectable boot is supported
-		EDD is supported
-		Japanese floppy for NEC 9800 1.2 MB is supported (int 13h)
-		Japanese floppy for Toshiba 1.2 MB is supported (int 13h)
-		5.25"/360 kB floppy services are supported (int 13h)
-		5.25"/1.2 MB floppy services are supported (int 13h)
-		3.5"/720 kB floppy services are supported (int 13h)
-		3.5"/2.88 MB floppy services are supported (int 13h)
-		8042 keyboard services are supported (int 9h)
-		CGA/mono video services are supported (int 10h)
-		ACPI is supported
-		USB legacy is supported
-		BIOS boot specification is supported
-		Targeted content distribution is supported
-		UEFI is supported
-	BIOS Revision: 0.26
-	Firmware Revision: 0.26
-
-Handle 0x0010, DMI type 13, 22 bytes
-BIOS Language Information
-	Language Description Format: Long
-	Installable Languages: 4
-		en|US|iso8859-1
-		fr|CA|iso8859-1
-		ja|JP|unicode
-		zh|TW|unicode
-	Currently Installed Language: en|US|iso8859-1
-
-    "#;
-        parse(sample);
-    }
-}
+mod tests;
